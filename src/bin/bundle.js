60ec54b560f5f1a37054b769c9db5670cb093ebc
Benjamim Sonntag <benjamimsonntag@gmail.com>
2017-12-09 00:52:12 +0000

    Add bundle script

---
+++ src/bin/bundle.js
@@ -0,0 +1,20 @@
+// @flow
+/* eslint-disable no-console */
+/* eslint-disable no-process-exit */
+
+import { renderClient } from 'client';
+import { renderToString } from 'react-dom/server';
+import Bluebird from 'bluebird';
+import fs from 'fs';
+
+const writeFile = Bluebird.promisify(fs.writeFile);
+
+const html = `<!DOCTYPE html>${renderToString(renderClient())}`;
+
+writeFile('dist/index.html', html)
+  .then(() => console.log('done'))
+  .catch(error => {
+    console.log(error);
+
+    process.exit(1);
+  });
