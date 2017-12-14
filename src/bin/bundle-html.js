722c29ca83e1ba6f7ef95303c70a6753ae79926c
Benjamim Sonntag <benjamimsonntag@gmail.com>
2017-12-14 20:59:45 +0000

    Update bundle html script

--- src/bin/bundle-html.js
+++ src/bin/bundle-html.js
@@ -7,11 +7,29 @@ import { renderToString } from 'react-dom/server';
 import Bluebird from 'bluebird';
 import fs from 'fs';
 
+type Page = {
+  fileName: string,
+  path: string,
+};
+
 const writeFile = Bluebird.promisify(fs.writeFile);
 
-const html = `<!DOCTYPE html>${renderToString(renderClient())}`;
+const pages: Array<Page> = [{
+  fileName: 'index.html',
+  path: '/',
+}, {
+  fileName: '404.html',
+  path: '/404',
+}];
 
-writeFile('dist/index.html', html)
+const renderPage = (page: Page) => {
+  const html = `<!DOCTYPE html>${renderToString(renderClient(page.path))}`;
+
+  return writeFile(`dist/${page.fileName}`, html);
+};
+
+Promise
+  .all(pages.map(renderPage))
   .catch(error => {
     console.log(error);
 
