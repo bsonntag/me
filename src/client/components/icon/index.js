4db433d63d0be86a161bfad763420209ac2e66f3
Benjamim Sonntag <benjamimsonntag@gmail.com>
2017-12-17 02:37:58 +0000

    Add link icon

--- src/client/components/icon/index.js
+++ src/client/components/icon/index.js
@@ -1,11 +1,13 @@
 // @flow
 
 import github from './github';
+import link from './link';
 import linkedIn from './linked-in';
 import twitter from './twitter';
 
 export default {
   github,
+  link,
   linkedIn,
   twitter,
 };
