89335e7816527cd7798e36c9286b331447e4b77e
Benjamim Sonntag <benjamimsonntag@gmail.com>
2017-12-23 21:55:12 +0000

    Update blog post with share options

--- src/client/components/pages/blog-post.js
+++ src/client/components/pages/blog-post.js
@@ -1,5 +1,6 @@
 // @flow
 
+import type { Config } from 'common/types';
 import type { Post } from 'blog';
 import type { RouteProps } from 'client/types';
 import { getBlogPost } from 'blog';
@@ -7,13 +8,16 @@ import { margin } from 'client/styles';
 import { renderDate } from 'client/utils';
 import Markdown from 'client/components/markdown';
 import React from 'react';
+import Share from 'client/components/share';
 import Type from 'client/components/type';
 import styled from 'styled-components';
+import withConfig from 'client/hocs/with-config';
 
 type Props = {
   blog: {
     posts: Array<Post>,
   },
+  config: Config,
 } & RouteProps;
 
 const Title = styled(Type.heading)`
@@ -24,7 +28,7 @@ const PostDate = styled(Type.paragraph)`
   ${margin.bottom('medium')}
 `;
 
-const BlogPost = ({ match }: Props) => {
+const BlogPost = ({ config, location, match }: Props) => {
   const { postId } = match.params;
   const { content, date, title } = getBlogPost(postId);
 
@@ -39,8 +43,10 @@ const BlogPost = ({ match }: Props) => {
       </PostDate>
 
       <Markdown source={content} />
+
+      <Share url={config.get('baseUrl') + location.pathname} />
     </article>
   );
 };
 
-export default BlogPost;
+export default withConfig(BlogPost);
