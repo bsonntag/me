ead97a14fd63486e13096f4d85d0836041de05d7
Benjamim Sonntag <benjamimsonntag@gmail.com>
2018-02-24 14:35:16 +0000

    Remove server bundler

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,8 @@
 // @flow
 /* eslint-disable no-console */
 
-import { bundleApplication } from 'server/bundler';
 import { renderClient } from 'client';
 import { renderToString } from 'react-dom/server';
-import config from 'config';
 import express from 'express';
 import morgan from 'morgan';
 
@@ -14,24 +12,14 @@ const app = express();
 
 app.use(morgan('tiny'));
 
-app.use('/assets', express.static('src/assets'));
+app.use('/static', express.static('build/client'));
 
-bundleApplication()
-  .then(bundle => {
-    if (config.get('javascriptEnabled')) {
-      app.get('/app.js', (request, response) => {
-        response.setHeader('content-type', 'application/javascript');
-        response.end(bundle);
-      });
-    }
+app.get('*', (request, response) => {
+  const html = renderToString(renderClient(request.url));
 
-    app.get('*', (request, response) => {
-      const html = renderToString(renderClient(request.url));
+  response.send(`<!DOCTYPE html>${html}`);
+});
 
-      response.send(`<!DOCTYPE html>${html}`);
-    });
-
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
-  });
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
